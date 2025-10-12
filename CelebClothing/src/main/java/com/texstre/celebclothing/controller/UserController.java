c0827010017bac96e68332e459b2b0e643e7b8ef
Vish369 <32028205+vish369jobs@users.noreply.github.com>
2025-10-12 21:53:06 +0530

    Additional Changes for OpenAPI
    Additional Changes for OpenAPI Spec base working

--- CelebClothing/src/main/java/com/texstre/celebclothing/controller/UserController.java
+++ CelebClothing/src/main/java/com/texstre/celebclothing/controller/UserController.java
@@ -1,9 +1,22 @@
 package com.texstre.celebclothing.controller;
 
+import com.texstre.model.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import com.texstre.api.UsersApi;
 
 @RestController
-public class UserController {
+public class UserController implements UsersApi {
 
+    @Override
+    public ResponseEntity<Void> createUser(User user) {
+        System.out.println("Create User API called");
+        return UsersApi.super.createUser(user);
+    }
 
+    @Override
+    public ResponseEntity<User> getUserById() {
+        System.out.println("getUserById API called");
+        return UsersApi.super.getUserById();
+    }
 }
